859106e1a8b9d560b804342914dd9a6e2dad204c
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-20 15:03:38 -0500

    Creates components from initial registration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+import {BrowserRouter, Match, Miss} from 'react-router'
+import TeamRegistration from './components/TeamRegistration'
+import NotFound from './components/NotFound'
+import App from './components/App'
+
+const Root = () => {
+	return(
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={TeamRegistration} />
+				<Match exactly pattern="/match/:matchId" component={App} />
+				<Miss component={NotFound} />
+			</div>
+		</BrowserRouter>
+	)	
+}
+
+render(<Root />, document.querySelector('#main'));
